2496d3057d970a2c5c5ab84ec907f2b714f6bebe
Denis-A7 <denis.a2023@vitstudent.ac.in>
2026-04-06 10:40:24 +0530

    Add unit test for Feed functionality

---
+++ test/src/java/FeedTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FeedTest{
+
+    @Test
+    public void testFeed(){
+        response = "It is good."
+        assertEquals(r1,response,"Same")
+    }
+}
